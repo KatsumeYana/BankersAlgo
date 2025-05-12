f0aee9ceb46cdceb5ea1e283198ce1a23b30b7c2
ch1llysauce <chillrigel05@gmail.com>
2025-05-12 21:46:01 +0800

    basta eto

--- BankersAlgo/src/bankersalgo/Dashboard.java
+++ BankersAlgo/src/bankersalgo/Dashboard.java
@@ -17,6 +17,8 @@ public class Dashboard extends javax.swing.JFrame {
         initComponents();
     }
 
+    private BankersAlgorithm bankersAlgo = new BankersAlgorithm();
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
